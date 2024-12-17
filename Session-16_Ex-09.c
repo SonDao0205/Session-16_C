b71c30e210861deece9bd8f78d4a27ce0874f840
sondt0205 <daotruongson2006@gmail.com>
2024-12-17 14:57:22 +0700

    Update Session-16_Ex-09.c

--- Session-16_Ex-09.c
+++ Session-16_Ex-09.c
@@ -1,23 +1,36 @@
 #include<stdio.h>
-void addValue(int numbers[],int index,int value){
-    int size = 7;
-    size++;
-    for (int i = size-1; i > index; i--)
-    {
-        numbers[i] = numbers[i-1];
-    }
-    numbers[index] = value;
+#include<stdlib.h>
+int addValue(int *arr,int *size,int index,int value);
+int main(){
+    int arr[7] = {1,2,3,4,5,6,7};
+    int size = sizeof(arr)/sizeof(int);
+    int index,value;
     for (int i = 0; i < size; i++)
     {
-        printf("numbers[%d] = %d\n",i, numbers[i]);
-    }    
-}
-int main(){
-    int numbers[7] = {1,2,3,4,5,6,7};
-    int index,value;
+        printf("arr[%d] = %d\n",i,arr[i]);
+    }   
     printf("Nhap gia tri muon them : ");
     scanf("%d",&value);
     printf("Nhap vi tri muon them : ");
     scanf("%d",&index);
-    addValue(numbers,index,value);
+    addValue(arr,&size,value,index);
+}
+int addValue(int *arr,int *size,int value, int index){
+    if (index < 0 || index >= *size)
+    {
+        printf("Vi tri khong hop le!\n");
+        return 0;
+    }
+    (*size)++;
+    for (int i = *size; i >= index; i--)
+    {
+        *(arr+i) = *(arr+i-1);
+    }
+    *(arr+index) = value;
+    printf("\n");
+    for (int i = 0; i < *size; i++)
+    {
+        printf("arr[%d] = %d\n",i,*(arr+i));
+    }   
+    return 0;
 }
